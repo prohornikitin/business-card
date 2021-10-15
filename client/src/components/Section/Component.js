220c1852c8a3a673c4b6d2fc90dce6505be5fefc
Prokhor40 <prohornikitin@gmail.com>
2021-10-15 11:47:25 +0300

    feature(Section property 'title')

--- client/src/components/Section/Component.js
+++ client/src/components/Section/Component.js
@@ -2,9 +2,12 @@ import React from "react";
 import s from './style.scss'
 import classnames from 'classnames'
 
-export default function Section({children, id, className, style}) {
+export default function Section({title, children, id, className, style}) {
     return (
-        <section className={classnames(s.main, className)} id={id} style={style}>
+        <section className={classnames(title ? s.main_with_title : s.main_no_title, className)} id={id} style={style}>
+            {
+                title ? <h2 className={s.title}>{title}</h2> : ""
+            }
             <div className={s.content}>
                 {children}
             </div>
