a69bc28fed4c5a215b68abcc49064d08032fc918
Prokhor40 <prohornikitin@gmail.com>
2021-10-23 11:01:25 +0300

    feature(FileUpload now has check_mark if file is selected)
    fix(FileUpload has onChange, style, className)

--- client/src/components/FileUpload/Component.js
+++ client/src/components/FileUpload/Component.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react'
 import s from './style.scss'
+import classnames from 'classnames'
+import check_mark from '../../icons/check_mark.png'
 
 
-export default function FileUpload({required, children}) {
+export default function FileUpload({className, style, required, onChange, children}) {
+    const [fileChose, setFileChose] = useState(undefined)
+
+    const internalOnChange = (e) => {
+        setFileChose(true)
+        onChange(e)
+    }
+
     return (
-        <label className={s.label}>
+        <label className={classnames(className, s.label)} style={style}>
             {children}
-
+            
             {required ?
-                <input required type="file" className={s.input}/> 
+                <input required type="file" onChange={internalOnChange} className={s.input}/> 
                 :
-                <input type="file" className={s.input}/>
+                <input type="file" onChange={internalOnChange} className={s.input}/>
+            }
+
+            {
+                fileChose ? <img className={s.success_img} src={check_mark}></img> : ""
             }
         </label>
-    );
+    )
 }
