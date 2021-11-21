ed1854d29a4b4b4adacb0494477561ddb80e438c
Prokhor40 <prohornikitin@gmail.com>
2021-11-21 14:11:52 +0300

    redesign portfolio readmoreDialog

--- client/src/components/Portfolio/ReadMoreDialog/Component.js
+++ client/src/components/Portfolio/ReadMoreDialog/Component.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import s from './style.scss'
 import Modal from 'react-modal'
-    
+
 
 export default function ReadMoreDialog({isOpen, onRequestClose, data}) {
     const style = {
         overlay: {
-            backgroundColor: "000000AA"
+            backgroundColor: "#00000099"
         },
         content: {
             boxSizing: "border-box",
             border: "none",
             borderRadius: "20px",
             backgroundColor: "#333",
-            inset: "200px",
-            width: "1200px",
+            width: "800px",
             margin: "auto",
+            padding: "0",
             height: "fit-content",    
         }
     }
@@ -30,16 +30,15 @@ export default function ReadMoreDialog({isOpen, onRequestClose, data}) {
             <div className={s.container}>
                 <img src={data.image} className={s.image}/>
                 <div className={s.text}>
-                    <h3>Technologies:</h3>
-                    <ul>{techList}</ul>
-                    <h3 style={{display:"inline"}}>Description: </h3>
-                    <p>{data.description}</p>
-                    <p>Link: <a href={data.link}>{data.link}</a></p>
+                    <div>{data.description}</div>
+                </div>
+                <div style={{margin:"16px auto", width: "fit-content", height: "fit-content"}}>
+                    <a className={s.link} href={data.link}>
+                        <svg style={{verticalAlign: "top"}} aria-hidden="false" width="14pt" height="14pt" viewBox="0 0 24 24"><path fill="currentColor" d="M10 5V3H5.375C4.06519 3 3 4.06519 3 5.375V18.625C3 19.936 4.06519 21 5.375 21H18.625C19.936 21 21 19.936 21 18.625V14H19V19H5V5H10Z"></path><path fill="currentColor" d="M21 2.99902H14V4.99902H17.586L9.29297 13.292L10.707 14.706L19 6.41302V9.99902H21V2.99902Z"></path></svg>
+                        &ensp; Link
+                    </a>
                 </div>
             </div>
-            
-            {/* <p>{data.description}</p> */}
-            <div className={s.closeButton} onClick={onRequestClose}/>
         </Modal>
     );
 }
