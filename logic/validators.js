a16edadf646d5af3095f0f2e117cf92024947388
Prokhor40 <prohornikitin@gmail.com>
2021-11-27 14:58:59 +0300

    fix: authData work validator when login and password located in body root

--- logic/validators.js
+++ logic/validators.js
@@ -3,6 +3,8 @@ const { check, buildCheckFunction } = require('express-validator');
 function authData(root = undefined) {
     if(root) {
         root += '.'
+    } else {
+        root = ""
     }
     return [
         check(`${root}login`).exists({checkFalsy: true}).isString(),
